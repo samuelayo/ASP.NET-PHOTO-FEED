6c8f83919889bb81e75012d8bfa43e520e7d25c1
samuel <samuel@WIN-CB74IDCL82O>
2017-08-02 21:35:32 +0100

    Add project files.

---
+++ Real-time-photo-feed/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Real_time_photo_feed.MvcApplication" Language="C#" %>
---
+++ Real-time-photo-feed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Real_time_photo_feed.Startup))]
+namespace Real_time_photo_feed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
